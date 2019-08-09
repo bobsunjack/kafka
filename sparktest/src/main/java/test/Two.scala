12d6af7477cfbd8ccccc22ee2cbf56ce389b36e5
qxb <qq@mail.com>
2019-08-09 15:14:08 +0800

    12.11编译

---
+++ sparktest/src/main/java/test/Two.scala
@@ -0,0 +1,33 @@
+package test
+
+import com.mongodb.spark.MongoSpark
+import org.apache.spark.sql.SparkSession
+
+object Two extends App {
+
+
+  val spark = SparkSession.builder()
+    .master("local")
+    .appName("MyApp")
+   // .config("spark.mongodb.input.uri", "mongodb://192.168.0.104/capturedb.user")
+    .getOrCreate()
+
+  // 设置log级别
+  spark.sparkContext.setLogLevel("WARN")
+
+/*
+
+  val df = MongoSpark.load(spark)
+  df.show()
+
+  df.createOrReplaceTempView("user")
+
+  val resDf = spark.sql("select name,creationDate from user")
+  resDf.show()
+*/
+
+  spark.stop()
+
+  System.exit(0)
+
+}
